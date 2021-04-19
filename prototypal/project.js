43fca98f47ede9ed72c4979078d80694deb181da
your name here <abirmahamud610@gmail.com>
2021-04-19 17:24:06 -0400

    Finish protypal final problem

--- prototypal/project.js
+++ prototypal/project.js
@@ -3,7 +3,33 @@ Function.prototype.inherits = function(SuperClass) {
     Surrogate.prototype = SuperClass.prototype;
     this.prototype = new Surrogate();
     this.prototype.constructor = this;
+
 }
 
 //Ship.inherits(MovingObject)
-//
+
+
+Function.prototype.inherits2 = function (SuperClass) {
+    this.prototype = Object.create(SuperClass);
+    this.prototype.constructor = this;
+
+
+}
+
+function movingObject () {};
+movingObject.prototype.move = function() {
+    console.log("Moving");
+}
+
+function Ship () {};
+
+function Asteroid () {};
+
+Ship.inherits2(movingObject);
+Asteroid.inherits2(movingObject);
+
+let s = new Ship();
+let a  = new Asteroid();
+
+s.move();
+a.move();
